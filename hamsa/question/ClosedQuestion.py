f8e41881864078cf143821d1d2afeac4a888e12f
Bruno Ferraz <brunoferraz.pro@gmail.com>
2020-08-15 11:53:53 -0300

    [RF02 - 02] saving backup to change modules structure

--- hamsa/question/ClosedQuestion.py
+++ hamsa/question/ClosedQuestion.py
@@ -2,8 +2,8 @@
 # from .. import question
 # # from ..question import IQuestion
 # from ...hamsa import question
+import hamsa as hs
 
-from ..question import IQuestion
 # class IQuestion():
 #     def __init__(self):
         
@@ -12,7 +12,7 @@ from ..question import IQuestion
 # class ClosedQuestion(question.IQuestion):
 #     def __init__(self, value, _label = None, _type = None, _question = None):
 #         pass
-class ClosedQuestion(IQuestion.Abstract):
+class ClosedQuestion(hs.question.IQuestion):
     def __init__(self):
         pass
     pass
--- hamsa/question/__init__.py
+++ hamsa/question/__init__.py
@@ -1,2 +1,6 @@
 # from . import IQuestion, ClosedQuestion, OpenedQuestion
-from . import(IQuestion, ClosedQuestion, OpenedQuestion) 
+from . import(ClosedQuestion, OpenedQuestion) 
+
+class IQuestion:
+    def __init__(self):
+        pass
